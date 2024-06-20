8ebb049c722bb808b1fbc96186775289393e8406
t-bolt <isholasobuur@gmail.com>
2024-06-20 18:26:00 +0100

    hope this fixes the issue

--- lib/src/video_watermark.dart
+++ lib/src/video_watermark.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
+
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit_flutter/session_state.dart';
 import 'package:flutter/material.dart' show ValueSetter, debugPrint;
-import 'package:ffmpeg_kit_flutter_min/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter_min/return_code.dart';
-import 'package:ffmpeg_kit_flutter_min/session_state.dart';
 import 'package:path_provider/path_provider.dart'
     show getApplicationDocumentsDirectory;
+
 import 'output_format.dart';
 import 'trim_video.dart';
 import 'watermark.dart';
